2f075fa3fa672951a122d58867dbf347ebe85140
alona <aloyna.vasilieva@gmail.com>
2024-10-08 22:01:34 +0200

    start navmenu

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import s from './index.module.css'
-import Logo from '../Logo'
+import React, { useState } from "react";
+import s from "./index.module.css";
+import Logo from "../Logo";
+import NavMenu from "../NavMenu";
 
 const Header = () => {
   return (
     <header className={s.header}>
-        <div className="container">
-            <div className={s.header_area}>
-                <Logo />
-            </div>
-
+      <div className="container">
+        <div className={s.header_area}>
+          <Logo />
+          <NavMenu />
         </div>
-        
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
