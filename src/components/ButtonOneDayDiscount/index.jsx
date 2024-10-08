fd41bee3c493d2cd01661445e11766389c1c1181
alona <aloyna.vasilieva@gmail.com>
2024-10-08 18:13:51 +0200

    create button dayli discount

---
+++ src/components/ButtonOneDayDiscount/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import s from './index.module.css'
+
+const ButtonOneDayDiscount = () => {
+  return (
+    <div>
+        <button className={s.btn}>
+        1 day discount!
+        </button>
+    </div>
+  )
+}
+
+export default ButtonOneDayDiscount
