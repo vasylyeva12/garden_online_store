8bf505f6bb888b19f8d945c94242a3a819a31dd4
alona <aloyna.vasilieva@gmail.com>
2024-10-08 16:05:13 +0200

    added routing and layout

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { Provider } from 'react-redux'
+import { store } from "./store/store.js";
+import { BrowserRouter } from 'react-router-dom'
 
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-createRoot(document.getElementById('root')).render(
-  
-    <App />
-  
-)
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
