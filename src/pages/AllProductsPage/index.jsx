bc0f2679c4ef828606122f23b0c2cb821a794e92
alona <aloyna.vasilieva@gmail.com>
2024-10-08 14:42:37 +0200

    create pages

---
+++ src/pages/AllProductsPage/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import s from './index.module.scss'
+
+const AllProductsPage = () => {
+  return (
+    <div>AllProductsPage</div>
+  )
+}
+
+export default AllProductsPage
---
+++ src/pages/AllSalesPage/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import s from './index.module.scss'
+
+const AllSalesPage = () => {
+  return (
+    <div>AllSalesPage</div>
+  )
+}
+
+export default AllSalesPage
