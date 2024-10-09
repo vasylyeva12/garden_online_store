fd02296924f579e74b8f866871b607b552619d66
alona <aloyna.vasilieva@gmail.com>
2024-10-09 23:07:31 +0200

    added actions and requests

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,10 @@ import Layout from './components/Layout'
 import AllSalesPage from './pages/AllSalesPage'
 import CategoriesPage from './pages/CategoriesPage'
 import NotFoundPage from './pages/NotFoundPage'
-import Footer from './components/Footer';
+import SingleCategoryPage from './pages/SingleCategoryPage';
+import SingleProductPage from './pages/SingleProductPage';
+import ShoppingCartPage from './pages/ShoppingCartPage';
+import FavoritesPage from './pages/FavoritesPage';
 
 const App = () => {
   return (
@@ -16,8 +19,15 @@ const App = () => {
         <Route path='/' element={<Layout />}>
           <Route index element={<MainPage />}/>
           <Route path='products' element={<AllProductsPage />}/>
+          <Route path='products/:product_id' element={<SingleProductPage />}/>
+          <Route path='sales/:id' element={<SingleProductPage />}/>
           <Route path='sales' element={<AllSalesPage />}/>
           <Route path='catigories' element={<CategoriesPage />}/>
+          <Route path='catigories/:category_name' element={<SingleCategoryPage />}/>
+          <Route path='catigories/:category_name/:id' element={<SingleCategoryPage />}/>
+          <Route path='cart' element={<ShoppingCartPage />}/>
+          <Route path='favorites' element={<FavoritesPage />}/>
+          <Route path='favorites/:id' element={<SingleProductPage />}/>
           <Route path='*' element={<NotFoundPage />}/>
         </Route>
       </Routes>
--- src/pages/AllProductsPage/index.jsx
+++ src/pages/AllProductsPage/index.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import s from './index.module.css'
 import ProductsList from '../../components/ProductsList'
+import { useDispatch } from 'react-redux'
+import { getAllProducts } from '../../requests/products'
 
 const AllProductsPage = () => {
+  const allProductsState = useSelector(store => store.products)
+
+  const dispatch = useDispatch()
+  useEffect(()=>{
+    dispatch(getAllProducts)
+  },[])
   return (
     <div>
       <ProductsList />
