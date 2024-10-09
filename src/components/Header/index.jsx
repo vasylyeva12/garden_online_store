46cc53ce0f8e5fca1b570463eb160af169cafa18
alona <aloyna.vasilieva@gmail.com>
2024-10-09 17:45:37 +0200

    assembled the header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,14 +2,18 @@ import React, { useState } from "react";
 import s from "./index.module.css";
 import Logo from "../Logo";
 import NavMenu from "../NavMenu";
+import BurgerMenu from "../BurgerMenu";
 
 const Header = () => {
   return (
     <header className={s.header}>
       <div className="container">
-        <div className={s.header_area}>
-          <Logo />
+        <div className={s.header_section}>
+          <Logo className={s.logo} />
           <NavMenu />
+          <div>
+            <BurgerMenu />
+          </div>
         </div>
       </div>
     </header>
--- src/components/NavMenu/index.jsx
+++ src/components/NavMenu/index.jsx
@@ -1,55 +1,36 @@
 import React from "react";
 import s from "./index.module.css";
 import { NavLink } from "react-router-dom";
-import { IoMdHeartEmpty } from "react-icons/io";
-import { PiHandbagThin } from "react-icons/pi";
-import sun from '../../media/sun.svg'
-import moon from '../../media/moon.svg'
 import ButtonOneDayDiscount from "../ButtonOneDayDiscount";
-
 
 const NavMenu = () => {
   return (
-    <div className={s.wrapper}>   
-    <div>
-        <div>
-            <img src={sun} alt="sun" />
-            {/* <img src={moon} alt="moon" /> */}
-        </div>
-    </div>
-       
-        <div className={s.navbar_content}>
-            <ButtonOneDayDiscount className={s.discountBtn}/>
-                     
-            <ul className={s.nav}>
-              <li className={s.nav_item}>
-                <NavLink to="/" >
-                  Main Page
-                </NavLink>
-              </li>
-              <li className={s.nav_item}>
-                <NavLink to="/categories" >
-                  Categories
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to="/products" >
-                  All products
-                </NavLink>
-              </li>
-              <li className={s.nav_item}>
-                <NavLink to="/sales" className={s.nav_item}>
-                  All sales
-                </NavLink>
-              </li>
-            </ul>
-          </div>
-          <div className={s.navbar_actions}>
-          <IoMdHeartEmpty className={s.navbar_actions_icon}/>
-          <PiHandbagThin className={s.navbar_actions_icon}/>           
-          
-          </div>
-        
+    <div className={s.wrapper}>
+       <li className={s.btn}>
+          <ButtonOneDayDiscount  />
+          </li>
+      <div className={s.navbar_content}>
+        <ul className={s.nav}>
+          <li className={s.nav_item}>
+            <NavLink to="/">Main Page</NavLink>
+          </li>
+          <li className={s.nav_item}>
+            <NavLink to="/categories">Categories</NavLink>
+          </li>
+          <li className={s.nav_item}>
+            <NavLink to="/products">All products</NavLink>
+          </li>
+          <li className={s.nav_item}>
+            <NavLink to="/sales" className={s.nav_item}>
+              All sales
+            </NavLink>
+          </li>
+         
+        </ul>
+      </div>
+      <div className={s.btn_hidden}>
+      <ButtonOneDayDiscount  />
+      </div>
       
     </div>
   );
