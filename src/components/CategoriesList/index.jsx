8d8acbda2af6b73a0908ccb533945d19f76499b8
LEARSCAIA LUMINITA <luminita.bors90@mail.ru>
2024-10-14 19:43:54 +0200

    button navigation

--- src/components/CategoriesList/index.jsx
+++ src/components/CategoriesList/index.jsx
@@ -2,28 +2,6 @@ import React from "react";
 import CategoryItem from "../CategoryItem";
 import s from "./index.module.css";
 
-
-// export default function CategoriesList() {
-
-  
-//   const { categories } = useSelector((state) => state.categories);
-  
-//     return (
-//     <div className={s.categories_wrapper}>
-//       <div className="container">
-//       <div className={s.categories_list}>
-//       <Link 
-//       key={category.id}
-//       to={/categories/${category.id}}
-//       className={s.card_category}>
-//         <CategoryItem {...category} />
-
-//       </Link>
-//       </div>
-//       </div>
-//     </div>
-//   );
-// }
 function CategoriesList({ categories }) {
 
   // const { categories } = useSelector((state) => state.categories);
--- src/components/CategoryItem/index.jsx
+++ src/components/CategoryItem/index.jsx
@@ -1,19 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
-const CategoryItem = ({image, title}) => {
-  
-  const imageUrl = `http://localhost:3333/products/${image}`;
+const CategoryItem = ({ image, title }) => {
 
   return (
-    <div className={s.card}>
-      <div className={s.image_wrapper}>
-        <img src={imageUrl} alt={title} />
-      </div>
-      <p>{title}</p>
-
+    <div className={s.categories_wrapper}>
+      <Link to={`/categories/${id}`}>
+        <div className={s.image_wrapper}>
+          <img
+            className={s.images}
+            src={`http://localhost:3333/${image}`}
+            alt={title}
+          />
+        </div>
+        <p>{title}</p>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default CategoryItem
-
+export default CategoryItem;
