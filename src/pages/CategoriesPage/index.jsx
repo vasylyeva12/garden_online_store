237e6e4a19ca4a1a6ebe9296f0b527614773430b
luminita1990 <152989177+luminita1990@users.noreply.github.com>
2024-10-15 13:40:36 +0200

    Update index.jsx

--- src/pages/CategoriesPage/index.jsx
+++ src/pages/CategoriesPage/index.jsx
@@ -1,36 +1,6 @@
-// import React, { useEffect } from "react";
-// import { useDispatch, useSelector } from "react-redux";
-// import CategoriesList from "../../components/CategoriesList";
-// import { getCategories } from "../../requests/categories";
-// import ButtonNavigation from "../../components/ButtonNavigation";
-
-// const CategoriesPage = () => {
-//   const categoriesState = useSelector((state) => state.categories); // Выбираем наш массив с продуктами
-
-//   const dispatch = useDispatch();
-
-//   useEffect(() => dispatch(getCategories), []);
-
-//   const crumbs = [
-//     { label: "Main page", path: "/" },
-//     { label: "Categories", path: "/categories" },
-//   ];
-
-//   return (
-//     <div className="container">
-//       <ButtonNavigation crumbs={crumbs} />
-//       <h2 className={s.categories_section-title}>Categories</h2>
-//       <CategoriesList categories={categoriesState}/>
-//     </div>
-//   );
-// };
-
-// export default CategoriesPage;
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import CategoriesList from "../../components/CategoriesList";
-
-
 import s from "./index.module.css";
 
 const CategoriesPage = () => {
@@ -41,11 +11,6 @@ const CategoriesPage = () => {
   useEffect(() => {
     dispatch(getCategories); 
   }, []);
-
-  // const crumbs = [
-  //   { label: "Main page", path: "/" },
-  //   { label: "Categories", path: "/categories" },
-  // ];
 
   return (
     <div className="container">
